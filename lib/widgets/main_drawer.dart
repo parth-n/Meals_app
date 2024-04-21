dabc23d041d8035c1337cb686dcf553cc00caa8d
parth-n <parth.nepalia@gmail.com>
2024-04-22 00:26:26 +0530

    added Meals and Filters Section in Side Drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -28,6 +28,30 @@ class Maindrawer extends StatelessWidget {
                   )
                 ],
               )),
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text('Meals',
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24)),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.filter_alt,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text('Filters',
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24)),
+            onTap: () {},
+          ),
         ],
       ),
     );

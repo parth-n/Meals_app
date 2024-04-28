ea3379e74309c590bc314333506d3f142f302115
parth-n <parth.nepalia@gmail.com>
2024-04-28 23:36:15 +0530

    created provider for favorites

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -3,6 +3,24 @@ import 'package:meals_app/models/meal.dart';
 
 class FavoriteMealsNotfier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotfier() : super([]);
+
+  void toggleFavoriteMealsStatus(Meal meal) {
+    //not allowed to edit the object in existing memory, cannot use add/remove methods on List. always create a new memory.
+
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [
+        ...state,
+        meal
+      ]; //used spread operator to individually add all elements of state list to new list and then add meal to new state list.
+    }
+  }
 }
 
-final favoriteMealsProvider = StateNotifierProvider();
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotfier, List<Meal>>((ref) {
+  return FavoriteMealsNotfier();
+});

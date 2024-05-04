c304d2e488d247acc76f60a949115a2b70b0d608
parth-n <parth.nepalia@gmail.com>
2024-05-04 13:11:41 +0530

    added explicit animations

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,13 +5,41 @@ import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/categorygriditem.dart';
 import 'package:meals_app/models/category.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animateController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animateController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      //lowerBound: 0,
+      //upperBound: 1) these values are by default 0 and 1, can be used to set different values
+    );
+
+    _animateController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animateController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
     //it will push the route which will be a widget on top of current stack of screens.
@@ -24,22 +52,29 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children: [
-          //availableCategories.map((category) => CategoryGridItem(category: category)).toList() is an alternative for for loop
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onselectCategory: () {
-                _selectCategory(context, category);
-              },
-            )
-        ]);
+    return AnimatedBuilder(
+        animation: _animateController,
+        child: GridView(
+            padding: const EdgeInsets.all(24),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20),
+            children: [
+              //availableCategories.map((category) => CategoryGridItem(category: category)).toList() is an alternative for for loop
+              for (final category in availableCategories)
+                CategoryGridItem(
+                  category: category,
+                  onselectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                )
+            ]),
+        builder: (context, child) => Padding(
+              padding:
+                  EdgeInsets.only(top: 100 - _animateController.value * 100),
+              child: child,
+            ));
   }
 }

4af171d01567712e5413ae60199f769cec6d8108
parth-n <parth.nepalia@gmail.com>
2024-05-05 15:35:05 +0530

    animated favorites button

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -28,8 +28,17 @@ class DetailScreen extends ConsumerWidget {
                         ? 'Meal added as a favorite'
                         : 'Meal removed from favorite')));
               },
-              icon: Icon(
-                  isFavorite ? Icons.star : Icons.star_border)) // favorite icon
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                      turns:
+                          Tween<double>(begin: 0.8, end: 1).animate(animation),
+                      child: child);
+                },
+                child: Icon(isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite)),
+              )) // favorite icon
         ],
       ),
       body: SingleChildScrollView(
